6b418c25cfcdefe2e2b028361a3cdbfc285555ec
chriskim2311 <chriskim2311@gmail.com>
2019-02-22 17:42:05 -0800

    completed fib, matrix problems

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,36 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        if(cache[args]){
+        return cache[args]
+        }
+
+        const result = fn.apply(this, args)
+        cache[args] = result
+        return result;
+    }
+}
+
+function slowFib(n) {
+    // const newArray =[0,1]
+    // for (let i = 2; i <= n; i++) {
+    //    const a = (newArray[ i - 2]) 
+    //    const b = (newArray[ i - 1])
+    //    newArray.push(a+b);
+    // }
+
+    // return newArray[n]
+
+    //exponential run time  
+    if(n < 2) {
+        return n
+    }
+    return fib(n-1) + fib(n-2)
+}
+
+const fib = memoize(slowFib)
 
 module.exports = fib;
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -1,7 +1,7 @@
 // --- Directions
 // Write a function that accepts an integer N
 // and returns a NxN spiral matrix.
-// --- Examples
+// --- Examples6
 //   matrix(2)
 //     [[1, 2],
 //     [4, 3]]
@@ -15,6 +15,51 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const results = [];
+
+    for (let i = 0; i < n; i++) {
+        results.push([])
+    }
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n-1;
+    let startRow = 0;
+    let endRow = n - 1;
+    while(startColumn <= endColumn && startRow <= endRow) {
+        for(let i = startColumn; i<= endColumn; i++) {
+            results[startRow][i] = counter;
+            counter++
+        }
+        startRow++
+
+
+        for (let i = startRow; i <= endRow; i++) {
+            results[i][endColumn] = counter;
+            counter++
+        }
+        endColumn--
+
+        for (let i =endColumn; i>= startColumn; i--) {
+            results[endRow][i] = counter
+            counter++
+        }
+        endRow--
+
+
+        for (let i =endRow; i >= startRow; i--) {
+            results[i][startColumn] = counter
+            counter++
+        }
+
+        startColumn++
+
+    }
+
+    return results
+
+
+
+}
 
 module.exports = matrix;
